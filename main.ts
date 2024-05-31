5394e34e71ce4ad41c3f2fa2a35e02fb0479e18b
ShJaveriajaved <161949130+ShJaveriajaved@users.noreply.github.com>
2024-05-31 17:42:54 +0500

    Add files via upload
    This is my repository of Word Counter.

---
+++ main.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.bold.cyanBright(" jj's Word Counter"));
+console.log("=".repeat(40));
+
+
+let answers = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter a Sentence",
+    }
+]);
+
+
+let words = answers.sentence.trim().split(" ");
+
+
+console.log("=".repeat(40));
+console.log(chalk.bold("-Sentence words:"));
+console.log(words);
+console.log(chalk.bold(` -Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(40));
+
